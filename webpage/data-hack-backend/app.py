f421bee1d05dbc64f214bd3c476b28437ebc6985
Tamalera <florence.meier@alumni.ethz.ch>
2020-08-29 10:51:28 +0200

    Bind BE and FE together with test functions

--- webpage/data-hack-backend/app.py
+++ webpage/data-hack-backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
-
+import sys
+import os
+import json
 
 # configuration
 DEBUG = True
@@ -12,8 +14,41 @@ app.config.from_object(__name__)
 # enable CORS
 CORS(app, resources={r'/*': {'origins': '*'}})
 
+# Create path to the project
+app_file_path = os.path.dirname(os.path.abspath(__file__))
+project_path = os.path.abspath(os.path.join(app_file_path, '..', '..'))
+# Add project path to the PYTHONPATH system variable
+sys.path.append(os.path.abspath(project_path))
+# Import private data set functions
+from src_private.load_process import load_private, data_preprocess
+# Load an preprocess the data
+df = data_preprocess(load_private("../../private_data/metervalues_anonymized.csv"))
+
+def get_private_ids(df):
+    """Get all the unique connectors"""
+    return df['chargepoint_connector'].unique().tolist()
+def test_plots(df, id):
+    """Test function"""
+    sub_df = df[df['chargepoint_connector'] == id]
+    return [{
+        'x': sub_df['timestamp'].values.tolist(),
+        'y': sub_df['increment'].tolist(),
+        'mode': 'lines',
+        'type': 'scatter',
+        'name': 'plot_1'
+    },
+    {
+        'x': sub_df['timestamp'].values.tolist(),
+        'y': (sub_df['increment'] + 1000).tolist(),
+        'mode': 'lines',
+        'type': 'scatter',
+        'name': 'plot_2'
+    }
+    ]
+
+
 def test_fct():
-    return {'x': [1, 2, 3], 'y': [10, 20, 30], 'type': 'bar'}
+    return [{'x': [1, 2, 3], 'y': [10, 20, 30], 'type': 'bar'}]
 
 @app.route('/', methods=['GET'])
 def index():
@@ -23,12 +58,9 @@ def index():
 def getById():
     if request.method == 'POST':
         id = request.get_json().get('id')
-        if(id == 1):
-            return jsonify({'x': [1, 5, 3], 'y': [10, 20, 100]})
-        else:
-            return jsonify({'x': [1, 5, 8], 'y': [13, 28, 21], 'type': 'bar'})
+        return json.dumps(test_plots(df, id))
     else: 
-        return jsonify({'x': [1, 2, 3], 'y': [10, 20, 30]})
+        return json.dumps(get_private_ids(df))
 
 
 if __name__ == '__main__':
