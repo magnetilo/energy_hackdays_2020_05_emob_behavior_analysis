ffa3ff4f1fe6387938272ff635c4f6e607aeb4ee
Tim Breitenbach <tim.breitenbach@axpo.com>
2020-08-29 11:38:29 +0200

    parsed hour_profile_per_region_per_weektime to json

--- public_temporal_data/csv_to_json.py
+++ public_temporal_data/csv_to_json.py
@@ -1,11 +1,11 @@
 import csv
 import json
 
-csvfile = open('occupied_ratio_per_municipality_typology.csv', 'r', encoding='utf-8')
-jsonfile = open('occupied_ratio_per_municipality_typology.json', 'w', encoding='utf-8')
+csvfile = open('hour_profile_per_region_per_weektime.csv', 'r', encoding='utf-8')
+jsonfile = open('hour_profile_per_region_per_weektime.json', 'w', encoding='utf-8')
 
-fieldnames = ("municipal_typologie_id","municipal_typologie","num_occupied","num_tot","occupied_ratio")
-reader = csv.DictReader(csvfile, fieldnames)
+#fieldnames = ("municipal_typologie_id","municipal_typologie","num_occupied","num_tot","occupied_ratio")
+reader = csv.DictReader(csvfile)
 for row in reader:
     json.dump(row, jsonfile)
     jsonfile.write('\n')
